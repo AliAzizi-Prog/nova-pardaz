e77af497c0dcba77dfce2263b3adf17cbec728a8
atena <atena.ganji.ag@gmail.com>
2024-01-16 22:52:03 +0330

    config for add photo

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,36 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
     loader: 'akamai',
-    path: '',
+    path: ''
   },
-  assetPrefix: './',
-};
 
-export default nextConfig;
+  assetPrefix: './',
+
+  webpack(config, { isServer }) {
+    // Use @svgr/webpack to handle SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts|md)x?$/]
+      },
+      use: [
+        '@svgr/webpack',
+        {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]'
+          }
+        }
+      ]
+    })
+
+    return config
+  }
+}
+
+export default nextConfig
